00ab6e9598a3d43f2bdeb7d7b5a657d0d98a065a
atrocityz <disbalance17@mail.ru>
2025-09-24 12:49:15 +0700

    refactor(day): edit badge and bg today colors

--- src/components/calendar/ui/day.tsx
+++ src/components/calendar/ui/day.tsx
@@ -19,7 +19,7 @@ function Day({
         'relative py-5 md:py-10 text-xl border-sidebar-border hover:opacity-70 not-[:nth-last-child(-n+7)]:border-b not-[:nth-child(7n)]:border-r transition-opacity',
         {
           'bg-muted/30': !isCurrentMonth,
-          'bg-green-300/20': isToday(date),
+          'bg-primary text-background': isToday(date),
         },
       )}
       onClick={onClick}
@@ -30,7 +30,7 @@ function Day({
         {date.getDate()}
       </time>
       {!!tasksCount && tasksCount > 0 && (
-        <span className="absolute md:bottom-3 md:right-3 bottom-1 right-0.5 rounded-full bg-accent-foreground text-background md:w-8 md:h-8 w-5 h-5 text-[14px] md:text-[16px] leading-none flex items-center justify-center">
+        <span className="absolute md:bottom-3 md:right-3 bottom-1 right-0.5 rounded-full bg-chart-2 text-background md:w-8 md:h-8 w-5 h-5 text-[14px] md:text-[16px] leading-none flex items-center justify-center">
           {tasksCount}
         </span>
       )}
