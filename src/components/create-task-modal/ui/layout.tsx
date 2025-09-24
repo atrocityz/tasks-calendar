c9025e6f53a7701db9b7d63c0f92787b55a8d4c4
atrocityz <disbalance17@mail.ru>
2025-09-24 18:30:26 +0700

    fix(create-task-modal): task list should be display only in calendar

--- src/components/create-task-modal/ui/layout.tsx
+++ src/components/create-task-modal/ui/layout.tsx
@@ -10,38 +10,36 @@ import type { ReactNode } from 'react'
 import { TaskTags } from '@/components/task-tags.tsx'
 
 export function Layout({
-  closeButton,
   tasks,
-  header,
   form,
+  showTaskList,
 }: {
-  closeButton: ReactNode
-  header: ReactNode
   form: ReactNode
   tasks: ReactNode
+  showTaskList: boolean
 }) {
   return (
-    <div className="w-[30%] min-w-[400px] relative top-[5%] left-1/2 max-h-[740px] -translate-x-1/2 border border-sidebar-border rounded-2xl bg-background p-6 flex flex-col gap-6">
-      {closeButton}
-      {header}
+    <div className="flex flex-col gap-6">
       {form}
-      <Drawer direction="right">
-        <DrawerTrigger className="self-center opacity-70 underline hover:opacity-100 transition-opacity">
-          Открыть список задач
-        </DrawerTrigger>
-        <DrawerContent className="p-3">
-          <DrawerHeader className="p-0 pb-2 gap-3 md:gap-4 mb-2">
-            <DrawerTitle className="text-xl text-center">
-              Список задач
-            </DrawerTitle>
-            <DrawerDescription className="sr-only">
-              Список задач на выбранный день
-            </DrawerDescription>
-            <TaskTags />
-          </DrawerHeader>
-          {tasks}
-        </DrawerContent>
-      </Drawer>
+      {showTaskList && (
+        <Drawer direction="right">
+          <DrawerTrigger className="self-center opacity-70 underline hover:opacity-100 transition-opacity">
+            Открыть список задач
+          </DrawerTrigger>
+          <DrawerContent className="p-3">
+            <DrawerHeader className="p-0 pb-2 gap-3 md:gap-4 mb-2">
+              <DrawerTitle className="text-xl text-center">
+                Список задач
+              </DrawerTitle>
+              <DrawerDescription className="sr-only">
+                Список задач на выбранный день
+              </DrawerDescription>
+              <TaskTags />
+            </DrawerHeader>
+            {tasks}
+          </DrawerContent>
+        </Drawer>
+      )}
     </div>
   )
 }
