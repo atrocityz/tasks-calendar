6cf8c67d76b167844b225f1b6bf64426a312ab0a
atrocityz <disbalance17@mail.ru>
2025-09-25 20:54:46 +0700

    fix(routing): github pages not support browser router

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
 import { Toaster } from '@/components/ui/sonner.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { CalendarPage } from '@/pages/calendar.page.tsx'
 import { TasksPage } from '@/pages/tasks.page.tsx'
 
 export function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Toaster />
       <Routes>
         <Route path="/" element={<CalendarPage />} />
         <Route path="/tasks" element={<TasksPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
